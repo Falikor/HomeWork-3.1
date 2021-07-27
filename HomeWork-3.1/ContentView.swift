655aceb34ad6dfd8332665f1270ecf42e8e2acf5
Victor <bugorv90@mail.ru>
2021-07-27 12:41:05 +0300

    Initial Commit

---
+++ HomeWork-3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork-3.1
+//
+//  Created by 17760021 on 27.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork-3.1/HomeWork_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_1App.swift
+//  HomeWork-3.1
+//
+//  Created by 17760021 on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
